327955832559c4c1e0cc2cdca564b3ecefee7017
321Proteus <bgproteus@gmail.com>
2024-06-09 13:43:00 +0200

    Reformat portfolio.js (whitespace is your friend)

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -52,36 +52,48 @@ $(".skip").click(() => {
 });
 
 $.fn.extend({
+
     animateCss: function (animationName) {
+
         const animationEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
         this.addClass(`animated ${animationName}`).one(animationEnd, () => {
             $(this).removeClass(`animated ${animationName}`);
         });
         return this;
     },
+
 });
 
 const writeLine = (text, speed, timeout, callback) => {
+
     timeout = typeof timeout === "number" ? timeout : [0, (callback = timeout)];
     const lineNumber = app.id !== 2 ? ++app.id : (app.id += 2);
+
     setTimeout(() => {
         const typed = new Typed(`#line${lineNumber}`, { strings: text, typeSpeed: speed, onComplete: callback });
     }, timeout);
+
 };
 
 const skipIntro = () => {
+
     if (app.skippedIntro) return;
     app.skippedIntro = true;
+
     timeouts.forEach((timeout) => {
         clearTimeout(timeout);
     });
+
     $(".top-right").remove();
     $("#start").fadeOut(100, () => {
+
         $("#start").remove();
         $("#marquee").marquee({ duration: 15000, gap: 420, delayBeforeStart: 1000, direction: "left", duplicated: true });
+
         setTimeout(() => {
             $(".brand-header").animateCss(app.effects[Math.floor(Math.random() * app.effects.length)]);
         }, 200);
+
         setTimeout(() => {
             const typed = new Typed("#brand", {
                 strings: app.brandDescription,
@@ -89,30 +101,40 @@ const skipIntro = () => {
                 loop: true
             });
         }, 1350);
+
         setTimeout(() => {
             $("#hiddenBox").fadeIn();
         }, 200);
+
     });
 };
+
 const clearCursor = () => {
     return $("span").siblings(".typed-cursor").css("opacity", "0");
 };
-var canvas = document.getElementById("canvas"),
-    ctx = canvas.getContext("2d");
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
 var stars = [],
     FPS = 60,
     x = 100,
     mouse = { x: 0, y: 0 };
+
 for (var i = 0; i < x; i++) {
     stars.push({ x: Math.random() * canvas.width, y: Math.random() * canvas.height, radius: Math.random() * 1 + 1, vx: Math.floor(Math.random() * 50) - 25, vy: Math.floor(Math.random() * 50) - 25 });
 }
 
 function draw() {
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.globalCompositeOperation = "lighter";
+
     for (var i = 0, x = stars.length; i < x; i++) {
+
         var s = stars[i];
         ctx.fillStyle = "#fff";
         ctx.beginPath();
@@ -121,48 +143,62 @@ function draw() {
         ctx.fillStyle = "black";
         ctx.stroke();
     }
+
     ctx.beginPath();
+
     for (var i = 0, x = stars.length; i < x; i++) {
+
         var starI = stars[i];
         ctx.moveTo(starI.x, starI.y);
         if (distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
+
         for (var j = 0, x = stars.length; j < x; j++) {
+
             var starII = stars[j];
+
             if (distance(starI, starII) < 150) {
                 ctx.lineTo(starII.x, starII.y);
             }
         }
     }
+
     ctx.lineWidth = 0.05;
     ctx.strokeStyle = "white";
     ctx.stroke();
 }
 
 function distance(point1, point2) {
-    var xs = 0;
-    var ys = 0;
-    xs = point2.x - point1.x;
-    xs = xs * xs;
-    ys = point2.y - point1.y;
-    ys = ys * ys;
+
+    var xs = point2.x - point1.x;
+    var ys = point2.y - point1.y;
+
+    xs *= xs;
+    ys *= ys;
+
     return Math.sqrt(xs + ys);
 }
 
 function update() {
+
     for (var i = 0, x = stars.length; i < x; i++) {
+
         var s = stars[i];
         s.x += s.vx / FPS;
         s.y += s.vy / FPS;
+        
         if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
         if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
     }
 }
+
 canvas.addEventListener("mousemove", function (e) {
+    
     mouse.x = e.clientX;
     mouse.y = e.clientY;
 });
 
 function tick() {
+
     draw();
     update();
     requestAnimationFrame(tick);
@@ -172,9 +208,9 @@ checkName(username).then(result => {
 
     username = result;
 
-    
     $.getJSON(ipgeolocation)
     .done((data) => {
+
         writeLine([lang[currentLang].intro.loading, resetPlaceholders(lang[currentLang].intro.loadingAccess, [["%username", username]])], 30, () => {
         if (app.skippedIntro) return;
 
@@ -183,31 +219,30 @@ checkName(username).then(result => {
         }
 
         clearCursor();
+
         const usernames = lang[currentLang].username.variants;
         const ip = data.ip ? data.ip : usernames[Math.floor(Math.random() * usernames.length)];
         const country = data.country_name ? data.country_name : lang[currentLang].intro.defaultCountry;
         const city = data.city ? data.city : lang[currentLang].intro.defaultCity;
+
         writeLine([lang[currentLang].intro.accessGranted, resetPlaceholders(lang[currentLang].intro.welcomeBack, [["%ip", ip], ["%city", city], ["%country", country]])], 30, 500, () => {
+
             if (app.skippedIntro) return;
             clearCursor();
             writeLine([`<i style='color: #F62459'>root@juljeryt.pl:~$ </i>`], 120, 500, () => {
-            timeouts.push(
-                setTimeout(() => {
-                if (app.skippedIntro) return;
-                clearCursor();
-                setTimeout(() => {
-                    skipIntro();
-                }, 500);
-                }, 1000)
-            );
+                timeouts.push(
+                    setTimeout(() => {
+                        if (app.skippedIntro) return;
+                        clearCursor();
+                        setTimeout(skipIntro, 500);
+                    }, 1000)
+                );
             });
         });
-        });
-    })
-    .fail((jqxhr, textStatus, error) => {
-        skipIntro();
     });
+    })
+    .fail(skipIntro);
 
-        tick();
+    tick();
 
 });
